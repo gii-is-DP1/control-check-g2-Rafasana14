652473c75b1bf8a29df8fbe5a5897e74cead0244
Rafael Sanabriagit <rafsanesp@alum.us.es>
2022-01-14 11:44:30 +0100

    Examen entregado

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
@@ -1,5 +1,64 @@
 package org.springframework.samples.petclinic.feeding;
 
+import java.util.Collection;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.PetService;
+import org.springframework.samples.petclinic.pet.PetType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class FeedingController {
+	
+	private final String VIEWS_FEEDINGS_CREATE_OR_UPDATE_FORM = "feedings/createOrUpdateFeedingForm";
+	
+	private final PetService petService;
+    private final FeedingService feedingService;
+
+    @Autowired
+    public FeedingController(PetService petService, FeedingService feedingService) {
+    	this.petService = petService;
+    	this.feedingService = feedingService;
+    }
+
+    @ModelAttribute("pets")
+    public Collection<PetType> populatePetTypes() {
+    	return this.petService.findPetTypes();
+    }
     
+    @ModelAttribute("feedingTypes")
+    public Collection<FeedingType> populateFeedingTypes() {
+    	return this.feedingService.getAllFeedingTypes();
+    }
+    
+    @GetMapping(value = "/feeding/create")
+	public String initCreationForm(ModelMap model) {
+		Feeding f = new Feeding();
+		model.put("feeding", f);
+		return VIEWS_FEEDINGS_CREATE_OR_UPDATE_FORM;
+	}
+
+	@PostMapping(value = "/feeding/create")
+	public String processCreationForm(@Valid Feeding feeding, BindingResult result, ModelMap model) {		
+		if (result.hasErrors()) {
+			model.put("feeding", feeding);
+			return VIEWS_FEEDINGS_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+			try{
+				this.feedingService.save(feeding);
+            }catch (UnfeasibleFeedingException e) {
+                model.put("message", "La mascota seleccionada no se le puede asignar el plan de alimentación especificado.");
+                return VIEWS_FEEDINGS_CREATE_OR_UPDATE_FORM;
+			}
+            return "redirect:/welcome";
+		}
+	}
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
@@ -3,9 +3,17 @@ package org.springframework.samples.petclinic.feeding;
 import java.util.List;
 import java.util.Optional;
 
-public interface FeedingRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FeedingRepository extends CrudRepository<Feeding, Integer>{
     List<Feeding> findAll();
-    // List<FeedingType> findAllFeedingTypes();
+    
+    @Query(value="SELECT ft FROM FeedingType ft ORDER BY ft.name")
+    List<FeedingType> findAllFeedingTypes();
     Optional<Feeding> findById(int id);
     Feeding save(Feeding p);
+    
+    @Query(value="SELECT ft FROM FeedingType ft WHERE ft.name=?1")
+    FeedingType findFeedingTypeByName(String name);
 }
